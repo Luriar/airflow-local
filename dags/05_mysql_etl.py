30edc2fdfbd96896b0aa8fab342027e0a11e7c98
Joonho Yoon <joonhoeric01@gmail.com>
2026-04-13 10:17:40 +0900

    Feat. temperatur_c 수정

--- dags/05_mysql_etl.py
+++ dags/05_mysql_etl.py
@@ -84,7 +84,7 @@ def _transform(**kwargs):
     pass
 
 def _load(**kwargs):
-    # csv => df => mysql 적제
+    # csv => df => mysql 적재
     # 1. csv 경로 획득 -> xcom을 통해서 이전 task(게시자)의 id를 이용하여 추출 <- ti 필요
     ti = kwargs['ti']
     csv_path = ti.xcom_pull(task_ids='transform')
@@ -96,6 +96,7 @@ def _load(**kwargs):
     mysql_hook = MySqlHook(mysql_conn_id='mysql_default')
     conn       = mysql_hook.get_conn() # 커넥션 획득 -> I/O 영향 있음(예외처리 등 필요, with문)
     # 7. 전체를 try ~ except로 감싸기(I/O)
+    # 실제는 실패 작업인데, 성공으로 오인할 수 있다 -> 예외 던지기 필요
     try:
         # 4. 커서를 획득하여 
         with conn.cursor() as cursor:        
@@ -107,7 +108,7 @@ def _load(**kwargs):
             '''
             # 여러 데이터를 한 번에 넣을 때 유용 -> executemany() 대응
             params = [
-                (data['sensor_id'], data['timestamp'], data['temperature_c'], data['temperature_f'])
+                (data['sensor_id'], data['timestamp'], data['temperature'], data['temperature_f'])
                 for _, data in df.iterrows() # 데이터가 없을 때까지 반복함 -> 데이터가 한세트씩 추출됨
             ]
             logging.info(f'입력할 데이터(파라미터) {params}')
@@ -117,7 +118,7 @@ def _load(**kwargs):
             logging.info('mysql에 적재 완료')
             pass        
     except Exception as e:
-        logging.info(f'적재 오류 : {e}')
+        logging.info(f'적재 오류 : {e}') # 예외 던지기 변경 필요 (리뷰 때)
         pass
     finally:
         # 5. 연결 종료
@@ -130,7 +131,7 @@ def _load(**kwargs):
 # 3. DAG 정의
 with DAG(
     dag_id      = "05_mysql_etl", 
-    description = "etl 수행하여 mysql에 온도 센서 데이터 적제",
+    description = "etl 수행하여 mysql에 온도 센서 데이터 적재",
     default_args= {
         'owner'             : 'de_2team_manager',        
         'retries'           : 1,
