25c09f2b79d1131ea067dd29671dcc05dcdd0976
Joonho Yoon <joonhoeric01@gmail.com>
2026-04-16 15:28:32 +0900

    Feat: app.py 완성

--- flink/app.py
+++ flink/app.py
@@ -5,6 +5,13 @@
 - 표준 SQL + AWS + Flink 특징점 추가됨 형태
 - Flink를 이용하면 데이터를 배치|스트리밍 등 어떤 방식이든 분석에 적합한 데이터 형태로 가공할 수 있음
 - 자바|스칼라|파이썬 + SQL 결합하여 처리 가능함
+
+- 원본데이터 -> KDS(input) -> Flink stock_input table -> Flink 연산/전송 
+        -> Flink stock_output table -> KDS(output) -> firehose -> s3(가공된 데이터)
+  
+  or (소규모라면 lambda 사용)
+
+- 원본데이터 -> KDS(input) -> lambda service(서버리스) -> KDS(output) -> firehose -> s3(가공된 데이터)
 '''
 
 import os
@@ -18,14 +25,62 @@ def main():
     setting = EnvironmentSettings.new_instance().in_streaming_mode().build()
     # SQL과 유사한 방식으로 데이터를 다룰 수 있는 객체
     t_env = TableEnvironment(setting)
-
+    '''
+        로그 원문 1개
+        {
+            'event_time': '2026-04-16T11:22:47.562211',
+            'ticker': 'NVIDIA',
+            'price': 432.33,
+            'volume': 40,
+            'trade_id': 2028174
+        }
+    '''
     # 2. 입력데이터에 대한 테이블 구성 (kds로부터 (INPUT) 데이터를 읽기 처리 -> 어딘가에 담는다 -> 테이블 )
     # 티커, 가격, 로그발생시간, ...
+    # 입력데이터에 대한 테이블에 kds가 연결되어 있어야함
+    t_env.execute_sql('''
+        create table stock_input (
+            event_time TIMESTAMP(3),
+            ticker STRING,
+            price DOUBLE,
+            WATERMARK FOR event_time AS event_time - INTERVAL '5' SECOND                      
+                      ) with (
+                      "connector" = "kinesis",
+                      "stream" = "de-ai-12-an2-kds-stock-input",
+                      "aws.region" = "ap-northeast-2",
+                      "scan.stream.initpos" = "LATEST",
+                      "format" = "json"
+                      )
+                      ''')
 
     # 3. 출력데이터에 대한 테이블 구성 (kds로부터 (OUTPUT) 데이터를 읽기 처리 -> 어딘가에 담는다 -> 테이블 )
     # 티커, 평균가격, 생성시간
-
+    # 출력데이터에 대한 테이블에 kds가 연결되어 있어야함
+    t_env.execute_sql('''
+        create table stock_output (
+            event_time TIMESTAMP(3),
+            ticker STRING,
+            price DOUBLE,
+                      ) with (
+                      "connector" = "kinesis",
+                      "stream" = "de-ai-12-an2-kds-stock-output",
+                      "aws.region" = "ap-northeast-2",
+                      "scan.stream.initpos" = "LATEST",
+                      "format" = "json"
+                      )
+                      ''')
+    
     # 4. 연산(전처리, 가공, 분석(요구사항에 맞게)처리한 형태) 및 전송(kds(Output))
+    t_env.execute_sql('''
+        INSERT INTO stock_output
+        SELECT
+            ticker,
+            AVG(price) as avg_price,
+            TUMBLE_END(event_time, INTERVAL '10' SECOND) as avg_time
+        from
+            stock_input
+        GROUP BY TUMBLE_END(event_time, INTERVAL '10' SECOND), ticker
+                      ''').wait()
     pass
 
 # 단독형 앱 -> 엔트리 포인트 표기 필요!
